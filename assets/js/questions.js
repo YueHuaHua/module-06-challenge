530e24668c7ab782894d99e64cff3f5634109699
YueHuaHua <bulanarum@gmail.com>
2023-01-24 14:38:32 +0000

    added question list

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        "title": "Common Data Types DO NOT include:",
+        "choices": ["String", "booleans", "alerts", "numbers"],
+        "answer": "alerts"
+    },
+    {
+        "title": "Conditionals keywords are:",
+        "choices": ["if", "for", "while", "for each"],
+        "answer": "if"
+    },
+    {
+        "title": "Corgis type include:",
+        "choices": ["welsh", "brown", "green", "purple"],
+        "answer": "welsh"
+    }
+]
