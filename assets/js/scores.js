f7c598cd4a5052dca67600c7921e2752ba2e6004
YueHuaHua <bulanarum@gmail.com>
2023-01-24 17:00:45 +0000

    fixed typo in printhighscores function

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,19 +1,17 @@
 function printHighScores() {
-    if(localStorage["highscores"]){
-        let highScores = JSON.parse(localStorage.getItem("highscores") || []);
-    
-        highScores.sort(function(a, b) {
-            return b.score -a.score;
-        })
+    let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-        highScores.forEach(function(score) {
-            let li = document.createElement("li");
-            li.textContent = `${score.initials} - ${score.score}`
+    highScores.sort(function(a, b) {
+        return b.score -a.score;
+    })
 
-            let ol = document.getElementById("highscores");
-            ol.appendChild(li);
-        })
-    }
+    highScores.forEach(function(score) {
+        let li = document.createElement("li");
+        li.textContent = `${score.initials} - ${score.score}`
+
+        let ol = document.getElementById("highscores");
+        ol.appendChild(li);
+    })
 }
 
 function clearHighScores(){
