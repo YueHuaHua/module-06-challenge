11f5b0c706592c5e4c7cea4b357354db1a6aa41c
YueHuaHua <bulanarum@gmail.com>
2023-01-24 16:01:35 +0000

    added printinghighscores function code

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,17 @@
 function printHighScores() {
+    let highScores = JSON.parse(localStorage.getItem("highscores") || []);
 
+    highScores.sort(function(a, b) {
+        return b.score -a.score;
+    })
+
+    highScores.forEach(function(score) {
+        let li = document.createElement("li");
+        li.textContent = `${score.initials} - ${score.score}`
+
+        let ol = document.getElementById("highscores");
+        ol.appendChild(li);
+    })
 }
 
 
